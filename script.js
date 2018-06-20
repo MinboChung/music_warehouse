6619ff3247ffcbb733fcbafd2e957aeac2df5ed2
Minbo Chung <Minbo_chung@hotmail.com>
2018-06-20 16:07:35 +0200

    Able to pause and play the music

--- script.js
+++ script.js
@@ -3,18 +3,35 @@ var outside = new Audio('1-02 Outside (Feat. Beenzino).mp3');
 var sometimes = new Audio('1-01 가끔.mp3');
 var forget = new Audio('1-01 잊어버리지마 (Feat. 태연).mp3');
 
+
 function play_audio(letter) {
     if (letter == 'Beautiful') {
-        beautiful.play();
+        if (!beautiful.paused) {
+            beautiful.pause();
+        } else {
+            beautiful.play();
+        }
     } else if (letter == 'Outside') {
-        outside.play();
+        if (!outside.paused) {
+            outside.pause();
+        } else {
+            outside.play();
+        }
     } else if (letter == 'Sometimes') {
-        sometimes.play();
+        if (!sometimes.paused) {
+            sometimes.pause();
+        } else {
+            sometimes.play();
+        }
     } else if (letter == 'Forget') {
-        forget.play();
-    } 
-    
-    else {
+        if (!forget.paused) {
+            forget.pause();
+        } else {
+            forget.play();
+        }
+    }  else {
         alert("No music can be found!");
     }
+
 }
+
