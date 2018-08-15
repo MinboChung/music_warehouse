30f8dcd252712a540a6d2658f4b8f8d9b04565c2
Minbo Chung <Minbo_chung@hotmail.com>
2018-08-15 11:54:38 +0200

    Add some pop up videos

--- script.js
+++ script.js
@@ -47,3 +47,32 @@ function play_audio(letter) {
     }
 };
 
+function onVideoClick(theLink) {
+    document.getElementById("video_pop").innerHTML = "<video autoplay muted loop id=\"the_Video\"><source src=\""+theLink+"\" type=\"video/webm\"></video>";
+    document.getElementById("video_pop").style.display="block";
+} 
+
+function onPopClick() {
+    document.getElementById("video_pop").style.display="none";
+    document.getElementById("video_pop").innerHTML = ""; 
+}         
+
+
+//https://youtu.be/Nkbz1te_lqI
+
+
+//Murtada code
+
+var modalButton = document.querySelector('.modal-button');
+var modalButtonClose = document.querySelector('.modal-button-close');
+var modalOverlay = document.querySelector('.modal-overlay');
+var modalIframe = document.querySelector('.modal-iframe');
+
+modalButton.addEventListener('click', function(event) {
+  modalOverlay.classList.add('is-open');
+  modalIframe.src = event.target.dataset.link;
+});
+
+modalButtonClose.addEventListener('click', function(event) {
+  modalOverlay.classList.remove('is-open');
+});
